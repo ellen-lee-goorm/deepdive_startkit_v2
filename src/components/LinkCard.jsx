2f78e9a7d9d37fac421440bdac05c40749aee4e6
ellen-lee-goorm <ellen.lee@goorm.io>
2025-11-16 00:38:24 +0900

    feat: 필수 진행 사항 스타일링

--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -3,7 +3,7 @@ import { IoIosArrowDropright } from 'react-icons/io';
 
 function LinkCard({ title, description, linkUrl, linkTitle, children }) {
   return (
-    <div className="flex gap-14 w-full text-start">
+    <div className="flex gap-14 w-full text-start text-[14px]">
       <section className="flex flex-col flex-1 bg-[var(--card-bg)] px-10 py-8 rounded-2xl">
         <div className="flex gap-4 items-center mb-6">
           <h3 className="text-[22px] font-bold">{title}</h3>
@@ -14,7 +14,7 @@ function LinkCard({ title, description, linkUrl, linkTitle, children }) {
       <a
         href={linkUrl}
         target="_blank"
-        className="max-w-16 flex flex-col justify-center items-center text-center gap-1"
+        className="max-w-16 flex flex-col justify-center items-center text-center gap-1 whitespace-pre-line"
       >
         <IoIosArrowDropright size={56} />
         <p className="font-bold">{linkTitle}</p>
--- src/layouts/RequireStep.jsx
+++ src/layouts/RequireStep.jsx
@@ -3,42 +3,89 @@ import LinkCard from '../components/LinkCard';
 
 function RequireStep() {
   return (
-    <article>
-      <span>필수 진행 사항</span>
-      <p>
+    <article className="flex flex-col items-center w-full text-[18px] py-24 border-b-[0.8px] border-b-[var(--line)]">
+      <span className="px-3 py-2 border-2 rounded-3xl text-sm font-bold text-[#539CCA] mb-7">
+        필수 진행 사항
+      </span>
+      <p className="leading-[21px] mb-11">
         과정 시작 전 반드시 완료해야 하는 필수 사항입니다.
         <br />
         아래 안내 드리는 필수 진행 사항은 지금 바로 진행해주세요!
       </p>
-      <LinkCard
-        title="고용24 수강신청"
-        description="과정 수강을 위해서는 고용24에서 수강신청을 완료해야 합니다. 아래 안내에 따라 수강신청을 완료해주세요."
-        linkUrl=""
-        linkTitle={`고용24 수강신청 하러가기`}
-      >
-        <div>
-          <h3>내일배움카드 발급</h3>
-          <ul>
-            <li>온라인 발급: 고용24에서 신청</li>
-            <li>
-              오프라인 발급: 은행 방문 후 발급 (신한은행 앱카드 신청 시 빠른
-              발급 가능)
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h3>고용24 수강신청</h3>
-          <ul>
-            <li>
-              고용24 수강신청 가이드를 따라 수강 신청 하기
-              <br />
-              <a href="" target="_blank">
-                고용24 수강신청 가이드 바로 가기
-              </a>
-            </li>
-          </ul>
-        </div>
-      </LinkCard>
+      <div className="flex flex-col gap-7">
+        <LinkCard
+          title="고용24 수강신청"
+          description="과정 수강을 위해서는 고용24에서 수강신청을 완료해야 합니다. 아래 안내에 따라 수강신청을 완료해주세요."
+          linkUrl=""
+          linkTitle={'고용24\n수강신청\n하러가기'}
+        >
+          <div className="flex gap-8 mb-4">
+            <h3 className="text-lg font-bold">내일배움카드 발급</h3>
+            <ul className="list-disc leading-[25px] ml-8">
+              <li>온라인 발급: 고용24에서 신청</li>
+              <li>
+                오프라인 발급: 은행 방문 후 발급 (신한은행 앱카드 신청 시 빠른
+                발급 가능)
+              </li>
+            </ul>
+          </div>
+          <div className="flex gap-8">
+            <h3 className="text-lg font-bold">고용24 수강신청</h3>
+            <ul className="list-disc leading-[25px] ml-8">
+              <li>
+                고용24 수강신청 가이드를 따라 수강 신청 하기
+                <br />
+                <a href="" target="_blank" className="underline">
+                  &gt; 고용24 수강신청 가이드 바로 가기
+                </a>
+              </li>
+            </ul>
+          </div>
+        </LinkCard>
+        <LinkCard
+          title="Discord 가입"
+          description="DEEP DIVE 과정은 Discord를 통해 모든 소통이 진행됩니다. 과정 안내 및 진행을 위해 Discord 서버를 가입해주세요."
+          linkUrl=""
+          linkTitle={'Discord\n가입하기'}
+        >
+          <div className="flex gap-8 mb-4">
+            <h3 className="text-lg font-bold">설정 가이드</h3>
+            <ul className="list-disc leading-[25px] ml-8">
+              <li>가입 후 반드시 아래 양식으로 닉네임을 변경해주세요</li>
+              <ul className="list-disc leading-[25px] ml-4">
+                <li>김구름(프론트엔드 9회차)</li>
+              </ul>
+            </ul>
+          </div>
+        </LinkCard>
+        <LinkCard
+          title="온라인 오리엔테이션 참석"
+          description="OT 참석 구글폼을 제출하고, 사전 준비사항을 미리 준비해주세요."
+          linkUrl=""
+          linkTitle={'OT 참석\n구글폼\n제출하기'}
+        >
+          <div className="flex gap-18 mb-4">
+            <h3 className="text-lg font-bold">OT 진행</h3>
+            <ul className="list-disc leading-[25px] ml-8">
+              <li>일정: 12월 19일(금) 9:00~18:00</li>
+              <li>장소: Zoom 강의실에서 진행 (링크는 추후 안내 예정)</li>
+            </ul>
+          </div>
+          <div className="flex gap-8">
+            <h3 className="text-lg font-bold">사전 준비사항</h3>
+            <ul className="list-disc leading-[25px] ml-8">
+              <li>고용24 앱 설치 및 실명인증 완료</li>
+              <li>
+                기기 등록
+                <ul className="list-disc leading-[25px] ml-4">
+                  <li>나의 출결 &gt; 기기 관리</li>
+                </ul>
+              </li>
+              <li>PC에 Zoom 설치 및 카메라 테스트</li>
+            </ul>
+          </div>
+        </LinkCard>
+      </div>
     </article>
   );
 }
