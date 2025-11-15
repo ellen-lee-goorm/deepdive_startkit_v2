65e212d7716d3f35c1a1e462b076ecfbb81cc62d
ellen-lee-goorm <ellen.lee@goorm.io>
2025-11-15 21:09:30 +0900

    fix: tailwind 오류 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import './App.css';
+import Header from './layouts/Header';
 
 function App() {
-  return <></>;
+  return (
+    <div className="flex flex-col items-center text-center max-w-[1075px]">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
